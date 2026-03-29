00cd5407d1733a663adf5d1aa224916f49864af3
JR Presidente <265961240+curiosoinformaticoetico-cmd@users.noreply.github.com>
2026-03-29 11:15:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from quantum.quantum_simulation import run_quantum
+
 def main():
     print("Iniciando sistema de IA cuántica JR...")
+
+    resultado = run_quantum()
+
+    print("Resultado del sistema cuántico:")
+    print(resultado)
 
 if __name__ == "__main__":
     main()
